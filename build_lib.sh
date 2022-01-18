e8a575e758fcab69991675837e7d93dfd0b789f4
葛高召 <gaozhao.ge@huolala.cn>
2022-01-18 11:21:33 +0800

    升级到0.3.0

--- build_lib.sh
+++ build_lib.sh
@@ -10,49 +10,52 @@ echo ///                        ///
 echo /// 🚀开始延迟编译二进制库🚀  ///
 echo ///                       ///
 
-printCurDirInfo() {
-    echo ====== 😄目录信息😄 ======
-    path=`cd $(dirname $0);pwd -P`
-    echo the current path is:$path
-    for i in `ls`
-    do
-            echo $i
-    done
-    filename=`basename $0`
-    echo file name is:$filename
-    echo ====== 😄目录信息😄 ======
-}
-
-#脚本所在目录
-printCurDirInfo
-
 #进入工程根目录
 cd Example
-printCurDirInfo
 
 #workspace名、scheme名字
 PROJECT_NAME='ABC'
 BINARY_NAME="${PROJECT_NAME}"
 
+#进入工程根目录
+cd Example
+
 #framework路径
 INSTALL_DIR=../PodProducts
-rm -dr "${INSTALL_DIR}"
-mkdir $INSTALL_DIR
+if [ -d ${INSTALL_DIR} ];then
+    rm -rf ${INSTALL_DIR}
+else
+    mkdir -p $INSTALL_DIR
+fi
 
 #编译场地
-WRK_DIR=build
-BUILD_PATH=${WRK_DIR}
+BUILD_PATH="build"
 RE_OS="Release-iphoneos"
 RE_SIMULATOR="Release-iphonesimulator"
-DEVICE_DIR=${BUILD_PATH}/${RE_OS}/${BINARY_NAME}.framework
-SIMULATOR_DIR=${BUILD_PATH}/${RE_SIMULATOR}/${BINARY_NAME}.framework
+DEVICE_DIR_FOLDER=${BUILD_PATH}/${RE_OS}
+SIMULATOR_DIR_FOLDER=${BUILD_PATH}/${RE_SIMULATOR}
+DEVICE_DIR=${DEVICE_DIR_FOLDER}/${BINARY_NAME}.framework
+SIMULATOR_DIR=${SIMULATOR_DIR_FOLDER}/${BINARY_NAME}.framework
 
 #分别编译模拟器和真机的Framework
-xcodebuild -configuration "Release" -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${BINARY_NAME}" ONLY_ACTIVE_ARCH=NO -sdk iphoneos CONFIGURATION_BUILD_DIR="${WRK_DIR}/${RE_OS}" clean build
-xcodebuild -configuration "Release" -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${BINARY_NAME}" ONLY_ACTIVE_ARCH=NO ARCHS='i386 x86_64' VALID_ARCHS='i386 x86_64' -sdk iphonesimulator CONFIGURATION_BUILD_DIR="${WRK_DIR}/${RE_SIMULATOR}" clean build
+xcodebuild -configuration "Release" -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${BINARY_NAME}" ONLY_ACTIVE_ARCH=NO -sdk iphoneos CONFIGURATION_BUILD_DIR="${DEVICE_DIR_FOLDER}" clean build
+xcodebuild -configuration "Release" -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${BINARY_NAME}" ONLY_ACTIVE_ARCH=NO ARCHS='i386 x86_64' VALID_ARCHS='i386 x86_64' -sdk iphonesimulator CONFIGURATION_BUILD_DIR="${SIMULATOR_DIR_FOLDER}" clean build
+
+if [ -d "./${DEVICE_DIR}/" ];then
+    echo "exist ${DEVICE_DIR}"
+else
+    echo "termination | reason: not exist ${DEVICE_DIR}"
+    exit
+fi
+
+if [ -d "./${SIMULATOR_DIR}/" ];then
+    echo "exist ${SIMULATOR_DIR}"
+else
+    echo "termination | reason: not exist ${SIMULATOR_DIR}"
+    exit
+fi
 
 #合成fat库
-printCurDirInfo
 INSTALL_LIB_DIR=${INSTALL_DIR}/${BINARY_NAME}.framework
 if [ -d "${INSTALL_LIB_DIR}" ]
 then
@@ -63,7 +66,7 @@ cp -a "${DEVICE_DIR}/" "${INSTALL_LIB_DIR}/"
 lipo -create "${DEVICE_DIR}/${BINARY_NAME}" "${SIMULATOR_DIR}/${BINARY_NAME}" -output "${INSTALL_LIB_DIR}/${BINARY_NAME}"
 
 #删除编译产物
-rm -dr $WRK_DIR
+rm -rf $BUILD_PATH
 
 echo ///                        ///
 echo /// 🚀完成延迟编译二进制库🚀  ///
